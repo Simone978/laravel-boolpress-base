b71a58d2fdf62e57549284c30c3f728d8833c6bb
Simone <simonelega.sl@gmail.com>
2020-03-25 15:52:36 +0100

    Creazione e importazione dati fake riuscita

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Post;
+use Faker\Generator as Faker;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -9,8 +10,16 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $newpost = new Post;
+            $newpost->title = $faker->sentence($nbWords = 5, $asText = false);
+            $newpost->content = $faker->paragraph($nbSentences = 3, $variableNbSentences = true);
+            $newpost->category = $faker->word;
+            $newpost->data = '2020-3-24';
+            $newpost->save();
+        }
+        
     }
 }
