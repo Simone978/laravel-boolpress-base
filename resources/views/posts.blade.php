9ea5bb1a9752662b01ce91a431b08c9d3ac70322
Simone <simonelega.sl@gmail.com>
2020-03-25 18:03:07 +0100

    implementata funzione destroy

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,5 +6,10 @@
     <p>{{$post->content}}</p>
     <p>Categoria: {{$post->category}}</p>
     <p>Pubblicato il {{$post->data}}</p>
+    <form action="{{route('posts.destroy', $post->id)}}" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Cancella</button>
+    </form>
     @endforeach
 @endsection
